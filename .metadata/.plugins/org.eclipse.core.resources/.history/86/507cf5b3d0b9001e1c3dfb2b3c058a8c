fdfd5860f3e541fa05051fecedf4b3adeffdee57
appcoding-ux <iy398884@gmail.com>
2024-01-23 18:56:04 +0900

    first commit

---
+++ .metadata/.plugins/org.eclipse.core.resources/.history/86/507cf5b3d0b9001e1c3dfb2b3c058a8c
@@ -0,0 +1,7 @@
+package ex02;
+
+public class HelloWorld {
+	public static void main(String[] args) {
+		System.out.println("저는 메인 메서드입니다.");
+	}
+}
---
+++ test2/src/ex01/Test1.java
@@ -0,0 +1,15 @@
+package ex01;
+
+public class Test1 {
+
+	public static void main(String[] args) {
+		System.out.println("코드 수정");
+		
+		boolean frag = true;
+		
+		for(;frag == true;) {
+			
+		}
+	}
+
+}
